3f7b19e73c1f7b0445b2d7e35818fcf7fd7e9497
Alfath Nurul Hidayah <145639834+Alfathnurulhidayah@users.noreply.github.com>
2025-06-15 11:47:39 +0700

    Update products.blade.php

--- NdogAsinAnaManing/resources/views/products.blade.php
+++ NdogAsinAnaManing/resources/views/products.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'Produk - Toko Telur Asin Maknyus')
+@section('title', 'Produk - N'dog Asin Organik Tegal Ana Maning')
 
 @section('content')
 <h2 class="mb-4" data-aos="fade-down">Produk Unggulan Kami</h2>
@@ -8,9 +8,11 @@
 <div class="row g-4">
     @php
         $products = [
-            ['name' => 'Telur Asin Original', 'desc' => 'Rasa asli dengan kualitas terbaik.', 'image' => 'images/telur-asin1.jpg'],
-            ['name' => 'Telur Asin Pedas', 'desc' => 'Varian pedas untuk pecinta rasa kuat.', 'image' => 'images/telur-asin-pedas.jpg'],
-            ['name' => 'Telur Asin Kuning', 'desc' => 'Tekstur lembut dengan cita rasa khas.', 'image' => 'images/telur-asin-kuning.jpg'],
+            ['name' => 'N'dog Asin Mentahan', 'desc' => 'Rasa asli dengan kualitas terbaik.', 'image' => 'images/telur-asin1.jpg'],
+            ['name' => 'N'dog Asin Brungus', 'desc' => 'Varian pedas untuk pecinta rasa kuat.', 'image' => 'images/telur-asin-pedas.jpg'],
+            ['name' => 'N'dog Asin Garang', 'desc' => 'Tekstur lembut dengan cita rasa khas.', 'image' => 'images/telur-asin-kuning.jpg'],
+            ['name' => 'N'dog Asin Godog', 'desc' => 'Tekstur lembut dengan cita rasa khas.', 'image' => 'images/telur-asin-kuning.jpg'],
+            ['name' => 'N'dog Asin Garang Wedi', 'desc' => 'Tekstur lembut dengan cita rasa khas.', 'image' => 'images/telur-asin-kuning.jpg'],
         ];
     @endphp
 
